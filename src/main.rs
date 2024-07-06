d9a4fcb90aba0655fe14801c3408e317e2837bdd
Ethan Welborn <ethanwelborn@protonmail.com>
2024-07-06 14:10:23 -0500

    int-to-string now supports negative numbers

--- src/main.rs
+++ src/main.rs
@@ -33,6 +33,9 @@ fn medusa_int_to_string(context: &mut CompilerContext) {
     let loop_index = context.label_index;
     context.label_index += 1;
 
+    let skip_negation = context.label_index;
+    context.label_index += 1;
+
     let loop_index_2 = context.label_index;
     context.label_index += 1;
 
@@ -58,11 +61,20 @@ mov r10, 10
 ; Length of the string is stored in RCX
 xor rcx, rcx
 
+; If the number is negative, set r15 to 1, then make the number positive for the rest of the calculations
+xor r15, r15
+cmp r9, 0
+jge label_{loop_index}
+
+; The number is indeed negative
+mov r15, 1
+neg r9
+
 label_{loop_index}:
 
 ; Divide the int by 10 to strip off the next digit (stored in RDX by idiv)
-xor rdx, rdx
 mov rax, r9
+xor rdx, rdx
 idiv r10
 mov r9, rax
 
@@ -75,6 +87,16 @@ inc rcx
 ; Keep stripping digits from the int until it's empty
 cmp r9, 0
 jnz label_{loop_index}
+
+; Before we reverse the string, check and see if the number was negative - if it was, add a minus sign
+cmp r15, 1
+jne label_{skip_negation}
+
+; Add the minus sign to the end - it will be at the beginning once we reverse the string
+mov byte [r8 + rcx], 45 ; ASCII number for '-'
+inc rcx
+
+label_{skip_negation}:
 
 ; Edge case: if the int was only one digit long, we shouldn't try to reverse the string
 cmp rcx, 1
