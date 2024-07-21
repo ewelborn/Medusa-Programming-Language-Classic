bd651576656c147e911af72485abf41f6bfb8b4e
Ethan Welborn <ethanwelborn@protonmail.com>
2024-07-20 21:29:22 -0500

    Simple testing suite implemented
    Simple testing suite was implemented, added a "hello world" test to ensure basic compiler functionality.

--- src/main.rs
+++ src/main.rs
@@ -1159,6 +1159,57 @@ buffer_string resb 1024");
     return Ok(());
 }
 
+
+#[cfg(test)]
+mod tests {
+    use crate::compile_from_text;
+
+    // Retrieve all of the output from a compiled medusa program
+    fn compile_and_get_output(source_text: String) -> String {
+        compile_from_text(source_text, "test".to_string()).unwrap();
+
+        // File is available at test.exe
+        let output = std::process::Command::new("./test.exe").output().unwrap();
+        match output.status.code().unwrap() {
+            0 => {
+                String::from_utf8(output.stdout).unwrap()
+            },
+            _ => {
+                panic!("Failed to execute test.exe")
+            }
+        }
+    }
+
+    // Retrieve only the program-specific output from a compiled medusa program, with all headers
+    //  and footers stripped (i.e. Medusa 1.0 and Program ended are removed) and all formatting
+    //  characters stripped (i.e. \n and \0)
+    fn compile_and_get_stripped_output(source_text: String) -> String {
+        let output = compile_and_get_output(source_text);
+        
+        let header = regex::Regex::new(r"Medusa 1.0").unwrap();
+        let footer = regex::Regex::new(r"Program ended").unwrap();
+        let formatting_characters = regex::Regex::new(r"(\n|\u{0})").unwrap();
+
+        let output = header.replace(output.as_str(), "").to_string();
+        let output = footer.replace(output.as_str(), "").to_string();
+        let output = formatting_characters.replace_all(output.as_str(), "").to_string();
+
+        return output;
+    }
+
+    fn load_test_from_directory(test_name: &str) -> String {
+        std::fs::read_to_string(format!("./tests/{}", test_name)).unwrap()
+    }
+
+    #[test]
+    fn test_hello_world() {
+        assert_eq!(
+            compile_and_get_stripped_output(load_test_from_directory("hello_world.med")),
+            "Hello, world!"
+        );
+    }
+}
+
 fn main() {
     let args: Vec<String> = std::env::args().collect();
 
