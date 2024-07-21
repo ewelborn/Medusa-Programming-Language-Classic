214616fc359adba15d8d2c24df123c5345b10cd8
Ethan Welborn <ethanwelborn@protonmail.com>
2024-07-20 22:20:26 -0500

    Project restructure into lib.rs
    Project restructured into a library - the only public function is compile_from_text. Tests were also rearranged to better match the integration testing layout provided in the Rust book (Chapter 21.3)
    
    Also, a WIP string_to_float function is being committed, but is not in a working state.

---
+++ tests/common/mod.rs
@@ -0,0 +1,36 @@
+// Retrieve all of the output from a compiled medusa program
+pub fn compile_and_get_output(source_text: &str) -> String {
+    medusa_lang::compile_from_text(source_text, "test".to_string()).unwrap();
+
+    // File is available at test.exe
+    let output = std::process::Command::new("./test.exe").output().unwrap();
+    match output.status.code().unwrap() {
+        0 => {
+            String::from_utf8(output.stdout).unwrap()
+        },
+        _ => {
+            panic!("Failed to execute test.exe")
+        }
+    }
+}
+
+// Retrieve only the program-specific output from a compiled medusa program, with all headers
+//  and footers stripped (i.e. Medusa 1.0 and Program ended are removed) and all formatting
+//  characters stripped (i.e. \n and \0)
+pub fn compile_and_get_stripped_output(source_text: &str) -> String {
+    let output = compile_and_get_output(source_text);
+    
+    let header = regex::Regex::new(r"Medusa 1.0").unwrap();
+    let footer = regex::Regex::new(r"Program ended").unwrap();
+    let formatting_characters = regex::Regex::new(r"(\n|\u{0})").unwrap();
+
+    let output = header.replace(output.as_str(), "").to_string();
+    let output = footer.replace(output.as_str(), "").to_string();
+    let output = formatting_characters.replace_all(output.as_str(), "").to_string();
+
+    return output;
+}
+
+pub fn load_test_from_directory(test_name: &str) -> String {
+    std::fs::read_to_string(format!("./tests/{}", test_name)).unwrap()
+}
---
+++ tests/hello_world.rs
@@ -0,0 +1,15 @@
+mod common;
+
+#[test]
+fn test_hello_world() {
+    let program = r#"
+"Hello, world!" -> @;
+"#;
+
+    let expected_output = "Hello, world!";
+
+    assert_eq!(
+        common::compile_and_get_stripped_output(program),
+        expected_output
+    );
+}
